95aa9de24db585c952b02c3b93c75b3496d3727a
Otssn <88810729+Otssn@users.noreply.github.com>
2021-10-24 00:39:26 -0500

    add

--- lib/screen/error_screen.dart
+++ lib/screen/error_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
+
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:harry_potter_app/components/loader_component.dart';
+import 'package:harry_potter_app/main.dart';
 import 'package:harry_potter_app/screen/error_screen.dart';
 import 'package:harry_potter_app/screen/list_character_screen.dart';
 import 'package:harry_potter_app/screen/wait_screen.dart';
@@ -16,19 +19,23 @@ class ErrorScreen extends StatefulWidget {
 class _ErrorScreenState extends State<ErrorScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      child: Stack(children: <Widget>[
-        Image(
-          image: AssetImage('assets/error.gif'),
+    return Container(
+      child: Scaffold(
+          body: Container(
+        child: Container(
+          child: Stack(children: <Widget>[
+            Image(
+              image: AssetImage('assets/error.gif'),
+            ),
+            Center(
+              child: Text(
+                'No hay conexion a internet',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            )
+          ]),
         ),
-        Center(
-          child: Text(
-            'No hay conexion a internet',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        )
-      ]),
-    ));
+      )),
+    );
   }
 }
